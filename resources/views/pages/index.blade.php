7c085cf7675a40d479a549191c91d84884f083cd
Jack-170 <agavejack@hotmail>
2024-02-22 17:12:50 +0100

    add: controllers

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -3,16 +3,18 @@
     <title>Home</title>
 @endsection
 @section('content')
+
     <h1 class="text-danger fw-bolder">Projects:</h1>
-    {{-- <ul>
-
-        @foreach ($projects as $project )
-
-        <li>{{ $project -> title }}</li>
-
-        @endforeach
-
-    </ul> --}}
+    <div class="container">
+        <ul>
+            @foreach ($projects as $project)
+                <li>
+                    {{ $project->title }}
+                    <p class="text-danger">{{ $project->type->name }}</p>
+                </li>
+            @endforeach
+        </ul>
+    </div>
 
 
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,8 @@
 <?php
 
+use App\Http\Controllers\ProjectController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-
-    return view('pages.index');
-});
+Route::get('/', [ProjectController::class, 'index'])->name('project.index');
 
 
