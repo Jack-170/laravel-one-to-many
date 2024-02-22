60380c35023ed9a2b959ebf0db22dd985d626de0
Jack-170 <agavejack@hotmail>
2024-02-22 14:34:37 +0100

    feat: add projects table

---
+++ database/factories/ProjectFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Project>
+ */
+class ProjectFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'title' => fake()->unique()->sentence(4, false),
+            'description' => fake()->paragraph(3, false),
+        ];
+    }
+}
